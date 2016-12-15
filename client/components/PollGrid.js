26131f1555f56aa153d22bb1b78242f4f2a10b25
Drew Fleeman <drew.fleeman@gmail.com>
2016-12-15 17:34:35 -0600

    add masonryjs grid formatting for index page grid items

--- client/components/PollGrid.js
+++ client/components/PollGrid.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Poll from './Poll';
+var Masonry = require('react-masonry-component');
+
+var masonryOptions = {
+    transitionDuration: 0,
+    fitWidth: true
+};
 
 const PollGrid = React.createClass({
   render() {
     return (
-      <div>
+      <Masonry
+          className={'masonry-grid'} // default ''
+          elementType={'div'} // default 'div'
+          options={masonryOptions} // default {}
+          disableImagesLoaded={false} // default false
+          updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+      >
         {this.props.polls.map((poll, i) => <Poll {...this.props} key={i} i={i} poll={poll} />)}
-      </div>
+      </Masonry>
     )
   }
 });
